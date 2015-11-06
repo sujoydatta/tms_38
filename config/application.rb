c52762897d007aa007088649914af27c327acac6
MuhammadTamzid <muhammad.tamzid@gmail.com>
2015-11-06 17:05:29 +0600

    trainee add edit display delete signin signout

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,10 @@ module BaseApp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # comma separated(:yml_file_names)
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:en, :vn]
+    config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
