4f71cebb68ac5bcce7eb514faf8aee3ee8a82a6b
SSalekin <sjsalekin@gmail.com>
2015-11-12 14:12:26 +0600

    Update Subject Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :subject, inverse_of: :tasks
 
   validates :name, presence: true, length: { maximum: 20 }
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 50 }
   validates :task_order, presence: true, numericality: {only_integer: true,
-                                    greater_than_or_equal_to: 0}
+                                         greater_than_or_equal_to: 0}
   validates :subject, presence: true
 end
